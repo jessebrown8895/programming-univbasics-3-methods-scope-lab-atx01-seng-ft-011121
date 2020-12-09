59e4905c439e195d3fe96b623a24a51ab3f235ea
Jesse Brown <brownjesse8895@yahoo.com>
2020-12-09 22:40:18 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -18,9 +18,9 @@ end
 link
 
 def all_phrases
-  puts "It's-a me, Mario!"
-  puts 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts "It's Dangerous To Go Alone! Take This."
+  mario 
+  toadstool 
+  link
 end
 
 all_phrases
